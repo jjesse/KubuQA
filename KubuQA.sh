d4f4a69c5581d03e598d57c40ace0849f6fa2c7d
erdem <erdemonal11@outlook.com>
2025-12-04 13:13:39 +0100

    Feature: Validate input

--- KubuQA.sh
+++ KubuQA.sh
@@ -329,6 +329,57 @@ ISO_DOWNLOAD_DIR="$ISO_DOWNLOAD_DIR/$FLAVOR"
 VDI_FILEPATH="$HOME/VirtualBox VMs/$VM_NAME/$VM_NAME.vdi"
 
 }
+
+# Validate user provided settings and input
+validate_integer() {
+    local value="$1"
+
+    case $value in
+        ''|*[!0-9]*)
+            return 1
+            ;;
+        0)
+            return 1
+            ;;
+        *)
+            return 0
+            ;;
+    esac
+}
+
+validate_settings() {
+    # Validate CPU cores
+    if ! validate_integer "$VM_CPU_CORES"; then
+        kdialog --error "The number of CPU cores must be a positive integer. You provided: \"$VM_CPU_CORES\""
+        echo "The number of CPU cores must be a positive integer. You provided: \"$VM_CPU_CORES\""
+        exit 1
+    fi
+
+    # Validate RAM size
+    if ! validate_integer "$VM_RAM"; then
+        kdialog --error "The amount of RAM must be a positive integer (in MB). You provided: \"$VM_RAM\""
+        echo "The amount of RAM must be a positive integer (in MB). You provided: \"$VM_RAM\""
+        exit 1
+    fi
+
+    # Validate ISO download directory
+    if [ -n "$ISO_DOWNLOAD_DIR" ] && [ -e "$ISO_DOWNLOAD_DIR" ] && [ ! -d "$ISO_DOWNLOAD_DIR" ]; then
+        kdialog --error "The ISO download path exists but is not a directory: \"$ISO_DOWNLOAD_DIR\""
+        echo "The ISO download path exists but is not a directory: \"$ISO_DOWNLOAD_DIR\""
+        exit 1
+    fi
+
+    # Validate paravirtualization provider
+    case $PARAVIRT in
+        ""|none|kvm)
+            ;;
+        *)
+            kdialog --error "Invalid paravirtualization provider: \"$PARAVIRT\". Supported values are \"none\" or \"kvm\"."
+            echo "Invalid paravirtualization provider: \"$PARAVIRT\". Supported values are \"none\" or \"kvm\"."
+            exit 1
+            ;;
+    esac
+}
 # MAIN
 # ----
 
@@ -340,7 +391,6 @@ check_and_install_tool VBoxManage virtualbox
 show_intro_dialog
 
 # Parse command line arguments
-# TODO Validate the input
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --config)        if source "$2"; then
@@ -370,11 +420,13 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
+
 # Check whether various components exist. If not or if requested, (re)create
 choose_flavor
 check_existing_vm
 check_existing_vdi
 check_existing_iso
+validate_settings
 
 # Prompt the user to launch a test install using VirtualBox
 if kdialog --yesno "Launch a Test Install using Virtual Box?"; then
